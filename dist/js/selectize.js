b5755b1dc9fcdedca22ac74359fc922cbf96a8f5
trungld <vn.trungld@gmail.com>
2018-01-11 18:25:12 +0700

    Custom build

--- dist/js/selectize.js
+++ dist/js/selectize.js
@@ -2958,6 +2958,22 @@
 	
 	});
 	
+	Selectize.define('enter_key_submit', function (options) {
+	  var self = this
+	
+	  this.onKeyDown = (function (e) {
+	    var original = self.onKeyDown
+	    return function (e) {
+	      if (e.keyCode === 13) {
+	        self.trigger('submit')
+	        e.preventDefault()
+	      }
+	      return original.apply(this, arguments)
+	    }
+	  })()
+	})
+	
+	
 	Selectize.define('optgroup_columns', function(options) {
 		var self = this;
 	
